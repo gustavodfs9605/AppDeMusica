4a88d6eb64dcd895322379a80755bdea990b8831
gustavodfs9605 <gustavodfs9605@gmail.com>
2020-09-24 21:31:12 -0500

    Primera version de la actividad principal

--- app/src/main/java/com/example/tienda_de_musica/MainActivity.java
+++ app/src/main/java/com/example/tienda_de_musica/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.tienda_de_musica;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eventoClick();
+    }
+
+    public void eventoClick() {
+        Button botonCargar = (Button) findViewById(R.id.botonCargarArtista);
+        botonCargar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tv = findViewById(R.id.texto);
+                tv.setText("Holaa");
+            }
+        });
+
     }
 }
